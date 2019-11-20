47fc7c366a8703011a7862a1500fe416e6c3d690
Willemijn <willemijn@bresjer.nl>
2019-11-20 21:30:23 +0100

    Add solution #1 - practicing export default

--- employee.js
+++ employee.js
@@ -8,54 +8,44 @@ calculate employee bonuses
 calculate the total amount an employee can be reimbursed based on the total value of their health, housing, or wellness benefits
 WorkAround currently contains the data and functions in a single file, though they would like to modify the program so it makes use of JavaScript modules. Specifically, WorkAround you to demonstrate the different ways to export and import modules. */
 
-let salary = 100000;
-
-let payGrades = {
-  entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
-  midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
-  seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
-};
-
-function getCadre() {
-  if (salary >= payGrades.entryLevel.minSalary && salary <= payGrades.entryLevel.maxSalary) {
-    return 'entryLevel';
-  } else if (salary >= payGrades.midLevel.minSalary && salary <= payGrades.midLevel.maxSalary) {
-    return 'midLevel';
-  } else return 'seniorLevel';
-}
-
-function calculateTax() {
-  return payGrades[getCadre()].taxMultiplier * salary;
-}
-
-function getBenefits() {
-  return payGrades[getCadre()].benefits.join(', ');
-}
-
-function calculateBonus() {
-  return .02 * salary;
-}
-
-function reimbursementEligibility() {
-  let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
-  let totalBenefitsValue = 0; 
-  let employeeBenefits = payGrades[getCadre()].benefits;
-  for (let i = 0; i < employeeBenefits.length; i++) {
-    totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
+let Employee = {
+    salary: 100000
+  };
+  
+  let payGrades = {
+    entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
+    midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
+    seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
+  };
+  
+  Employee.getCadre = function getCadre() {
+    if (Employee.salary >= payGrades.entryLevel.minSalary && Employee.salary <= payGrades.entryLevel.maxSalary) {
+      return 'entryLevel';
+    } else if (Employee.salary >= payGrades.midLevel.minSalary && Employee.salary <= payGrades.midLevel.maxSalary) {
+      return 'midLevel';
+    } else return 'seniorLevel';
   }
-  return totalBenefitsValue;
-}
-
-function getEmployeeInformation(inputSalary) {
-  salary = inputSalary;
-  console.log('Cadre: ' + getCadre());
-  console.log('Tax: ' + calculateTax());
-  console.log('Benefits: ' + getBenefits());
-  console.log('Bonus: ' + calculateBonus());
-  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
-}
-
-getEmployeeInformation(10000);
-getEmployeeInformation(50000);
-getEmployeeInformation(100000);
-
+  
+  Employee.calculateTax = function calculateTax() {
+    return payGrades[Employee.getCadre()].taxMultiplier * Employee.salary;
+  }
+  
+  Employee.getBenefits = function getBenefits() {
+    return payGrades[Employee.getCadre()].benefits.join(', ');
+  }
+  
+  Employee.calculateBonus = function calculateBonus() {
+    return .02 * Employee.salary;
+  }
+  
+  Employee.reimbursementEligibility = function reimbursementEligibility() {
+    let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
+    let totalBenefitsValue = 0; 
+    let employeeBenefits = payGrades[Employee.getCadre()].benefits;
+    for (let i = 0; i < employeeBenefits.length; i++) {
+      totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
+    }
+    return totalBenefitsValue;
+  }
+  
+  export default Employee;
