82a0021efcc242b4c0b54415cf10916e8efb0a35
Willemijn <willemijn@bresjer.nl>
2019-11-20 19:51:06 +0100

    Add starter files

---
+++ employee.js
@@ -0,0 +1,61 @@
+/* WorkAround
+WorkAround is a human resources program that returns basic information about an employee, based on salary data. Using WorkAround, one can use an employee’s salary data to:
+
+return the cadre of the of the employee, in other words, whether this employee is entry level, mid level, or senior level.
+calculate employee tax rates
+return employee benefits
+calculate employee bonuses
+calculate the total amount an employee can be reimbursed based on the total value of their health, housing, or wellness benefits
+WorkAround currently contains the data and functions in a single file, though they would like to modify the program so it makes use of JavaScript modules. Specifically, WorkAround you to demonstrate the different ways to export and import modules. */
+
+let salary = 100000;
+
+let payGrades = {
+  entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
+  midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
+  seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
+};
+
+function getCadre() {
+  if (salary >= payGrades.entryLevel.minSalary && salary <= payGrades.entryLevel.maxSalary) {
+    return 'entryLevel';
+  } else if (salary >= payGrades.midLevel.minSalary && salary <= payGrades.midLevel.maxSalary) {
+    return 'midLevel';
+  } else return 'seniorLevel';
+}
+
+function calculateTax() {
+  return payGrades[getCadre()].taxMultiplier * salary;
+}
+
+function getBenefits() {
+  return payGrades[getCadre()].benefits.join(', ');
+}
+
+function calculateBonus() {
+  return .02 * salary;
+}
+
+function reimbursementEligibility() {
+  let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
+  let totalBenefitsValue = 0; 
+  let employeeBenefits = payGrades[getCadre()].benefits;
+  for (let i = 0; i < employeeBenefits.length; i++) {
+    totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
+  }
+  return totalBenefitsValue;
+}
+
+function getEmployeeInformation(inputSalary) {
+  salary = inputSalary;
+  console.log('Cadre: ' + getCadre());
+  console.log('Tax: ' + calculateTax());
+  console.log('Benefits: ' + getBenefits());
+  console.log('Bonus: ' + calculateBonus());
+  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
+}
+
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
+
